ee95ffca05706b0d8a9bdf976c1b19a5adb801bc
Ian Williams <ian.williams@radicalregard.com>
2019-11-08 10:55:48 -0700

    The Crimson Cube Rises
    Web application code for "Crimson Cube" disk inventory manager.

---
+++ SWDV235Project2/Project2/Project2/Borrower.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace Project2
+{
+    public partial class Borrower : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        protected void borrowerSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+
+                borrowerFirstName.Text = "";
+                borrowerLastName.Text = "";
+                borrowerEmail.Text = "";
+                confirm.Text = "Borrower Submission Recieved!";
+            }
+        }
+
+        protected void borrowerEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SWDV235Project2/Project2/Project2/Site1.Master.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project2
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Artist.aspx");
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Borrower.aspx");
+            
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Disk.aspx");
+
+        }
+    }
+}
